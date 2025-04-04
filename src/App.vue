509a90749aa1717fe2d359527d0431859945867e
kwkscream <spoonkicker@gmail.com>
2025-04-04 13:35:32 +0300

    added input field and img for navigation

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <script setup>
 import { ref } from "vue";
 import { Sheet } from "bottom-sheet-vue3";
+import InputFields from "./components/InputFields.vue"
+
 
 const visible = ref(false);
 </script>
 
 <template>
-  <button @click="visible = true">Click to show sheet</button>
   <div class="flex justify-center">
-    <div class="max-w-x min-w-[200px]">
+    <div class="max-w-x min-w-[200px] p-4">
       <input
-        class="rounded-full shadow-lg p-2 w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-lg border border-slate-200 px-4 py-4 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300  focus:shadow"
+        class="rounded-full shadow-lg w-full bg-transparent placeholder:text-slate-700 text-neutral-900 text-balance border border-slate-300 px-3 py-3 transition duration-300 ease focus:outline-none focus:border-slate-600 hover:border-slate-300 focus:shadow focus-visible:ring-0 focus-visible:ring-offset-0"
         placeholder="Where to?"
         @click="visible = true"
       />
+    </div>
+  </div>
+
+  <div
+    class="absolute left-0 right-0 bottom-0 bg-white rounded-t-3xl shadow-xl transition-transform duration-300 ease-in-out transfor"
+    @click="visible = 'true'"
+  >
+    <div class="h-6 w-full flex items-center justify-center cursor-pointer">
+      <div class="w-12 h-1 bg-gray-300 rounded-full"></div>
     </div>
   </div>
   <div>
@@ -24,10 +34,13 @@ const visible = ref(false);
         '--bottom-sheet-min-width': '70%',
         '-bottom-sheet-max-height': '80%',
         '--bottom-sheet-header-bar-background-color': 'rgb(209,213,219)',
-        '--bottom-sheet-backdrop-background-color' : 'rgba(0, 0, 0, 0)',
+        '--bottom-sheet-backdrop-background-color': 'rgba(0, 0, 0, 0)',
       }"
     >
-      Hello this is Sheet Content
+      <InputFields></InputFields>
+    <template #header>
+      <div class="mt-3 w-12 h-1 bg-gray-300 rounded-full"></div>
+    </template>
     </Sheet>
   </div>
 </template>
---
+++ src/components/InputFields.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { MapPin, Navigation } from "lucide-vue-next";
+</script>
+
+<template>
+	<div class="p-4">
+		<h2 class="text-xl font-bold">Book a Ride</h2>
+	</div>
+	<div class="flex items-center justify-center">
+		<div class="flex flex-col items-center relative pl-4">
+			<MapPin
+				size="12"
+				color="#ff8000"
+				class="absolute translate-y-[-14px] translate-x-[-1.5px]"
+			/>
+
+			<div
+				class="w-1 h-13 border-l-2 border-dashed border-gray-300"
+			></div>
+
+			<Navigation
+				size="12"
+				color="#ff8000"
+				class="absolute translate-y-[52px] translate-x-[-2.7px]"
+			/>
+		</div>
+		<div class="flex flex-col p-4 w-full gap-4">
+			<div class="w-full min-w-[200px]">
+				<input
+					class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-4 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+					placeholder="Pickup location"
+				/>
+			</div>
+			<div class="w-full min-w-[200px]">
+				<input
+					class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-4 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+					placeholder="Destination"
+				/>
+			</div>
+		</div>
+	</div>
+</template>
